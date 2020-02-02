de5a97fa0e38ff82a4ffa0fc75b8eb6a9bdfc9ab
yiz132 <yiz132@pitt.edu>
2020-02-01 20:24:08 -0500

    "start"

---
+++ src/main/java/part4third.java
@@ -0,0 +1,75 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.log4j.BasicConfigurator;
+
+import java.io.IOException;
+
+public class part4third {
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        @Override
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String s = value.toString();
+            int index_get = s.indexOf("GET");
+            int index_start = s.indexOf(" ",index_get)+1;
+            int index_end = s.indexOf(" ", index_start);
+            String url = s.substring(index_start,index_end);
+            word.set(url);
+            context.write(word, one);
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private Text maxWord = new Text();
+        private int max = 0;
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            if (sum > max) {
+                max = sum;
+                maxWord.set(key);
+            }
+        }
+
+        @Override
+        public void cleanup(Context context) throws IOException, InterruptedException {
+            context.write(maxWord, new IntWritable(max));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
+        Configuration conf = new Configuration();
+
+
+        Job job = Job.getInstance(conf, "part4third");
+        job.setJarByClass(part4third.class);
+
+        job.setMapperClass(part4third.TokenizerMapper.class);
+        job.setCombinerClass(part4third.IntSumReducer.class);
+        job.setReducerClass(part4third.IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ src/main/java/part4two.java
@@ -0,0 +1,68 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.log4j.BasicConfigurator;
+
+import java.io.IOException;
+
+public class part4two {
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        @Override
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String s = value.toString();
+            String match="10.153.239.5";
+            if(s.contains(match)) {
+                word.set(match);
+                context.write(word, one);
+            }
+
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
+        Configuration conf = new Configuration();
+
+
+        Job job = Job.getInstance(conf, "hit count2");
+        job.setJarByClass(part4two.class);
+
+        job.setMapperClass(part4two.TokenizerMapper.class);
+        job.setCombinerClass(part4two.IntSumReducer.class);
+        job.setReducerClass(part4two.IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
